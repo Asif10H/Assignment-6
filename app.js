519eab4f9b5230c7b8685bb818074f5800b4a29c
Asif10H <habibullahasif71@gmail.com>
2021-02-14 14:30:49 +0600

    fix id name

--- app.js
+++ app.js
@@ -31,8 +31,7 @@ const showImages = (images) => {
 const getImages = (query) => {
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
-    .then(data => showImages(data.hits)) // error for hitS 
-    //.then(data => console.log(data))
+    .then(data => showImages(data.hits))
     .catch(err => console.log(err))
 }
 
